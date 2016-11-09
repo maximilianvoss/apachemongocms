f3842efc78422ff3952b7de850e79472c72d67b4
Maximilian Voss <maximilian.voss@gmail.com>
2016-11-09 17:56:51 +0100

    * unification of the configuration parameter names

--- documentation/configuration.md
+++ documentation/configuration.md
@@ -7,11 +7,11 @@ To set up the database following schema can be used:
 
 **Documents**
 
-| Key                  | Type   | Example                    | Description                                             |   
-| -------------------- | ------ | -------------------------- | ------------------------------------------------------- |
-| DocMongoURI          | String | mongodb://localhost:27017/ | Setting the URI for the document's MongoDB              |
-| DocMongoDB           | String | mongocms                   | DB in which the document's data shall be stored         |
-| DocMongoCollection   | String | documents                  | Collection in which the document's data shall be stored |
+| Key                | Type   | Example                    | Description                                             |   
+| ------------------ | ------ | -------------------------- | ------------------------------------------------------- |
+| DocMongoURI        | String | mongodb://localhost:27017/ | Setting the URI for the document's MongoDB              |
+| DocMongoDB         | String | mongocms                   | DB in which the document's data shall be stored         |
+| DocMongoCollection | String | documents                  | Collection in which the document's data shall be stored |
 
 
 **Assets**
@@ -25,40 +25,40 @@ To set up the database following schema can be used:
 
 **User**
 
-| Key                  | Type   | Example                    | Description                                          |
-| -------------------- | ------ | -------------------------- | ---------------------------------------------------- |
-| UserMongoURI         | String | mongodb://localhost:27107/ | Setting the URI for the user's MongoDB               |
-| UserMongoDB          | String | mongocms                   | DB in which the users's data shall be stored         |
-| UserMongoCollection  | String | user                       | Collection in which the asset's data shall be stored |
+| Key                 | Type   | Example                    | Description                                          |
+| ------------------- | ------ | -------------------------- | ---------------------------------------------------- |
+| UserMongoURI        | String | mongodb://localhost:27107/ | Setting the URI for the user's MongoDB               |
+| UserMongoDB         | String | mongocms                   | DB in which the users's data shall be stored         |
+| UserMongoCollection | String | user                       | Collection in which the asset's data shall be stored |
 
 
 ## Input/Output parameter filter and mapper
 
 **Documents**  
 
-| Key                         | Type              | Example                | Description                                            |   
-| --------------------------- | ----------------- | ---------------------- | ------------------------------------------------------ |
-| DocumentPropInputWhitelist  | String[]          | parameter1             | White list of parameters which will be accepted        |
-| DocumentPropOutputWhitelist | String[]          | parameter2             | White list of parameters which will be in the response |
-| DocumentPropMapping         | (String String)[] | parent#child mappedKey | Transforms the database keys to a mapped key           |
+| Key                     | Type              | Example                | Description                                            |   
+| ----------------------- | ----------------- | ---------------------- | ------------------------------------------------------ |
+| DocumentPropWhitelistIn | String[]          | parameter1             | White list of parameters which will be accepted        |
+| DocumentPropWhitlistOut | String[]          | parameter2             | White list of parameters which will be in the response |
+| DocumentPropMapping     | (String String)[] | parent#child mappedKey | Transforms the database keys to a mapped key           |
 
 
 **Assets**
 
-| Key                               | Type              | Example                | Description                                            |   
-| --------------------------------- | ----------------- | ---------------------- | ------------------------------------------------------ |
-| AssetMetadataParamInputWhitelist  | String            | parameter1             | White list of parameters which will be accepted        |
-| AssetMetadataParamOutputWhitelist | String            | parameter2             | White list of parameters which will be in the response |
-| AssetMetadataParamMapping         | (String String)[] | parent#child mappedKey | Transforms the database keys to a mapped key           |
+| Key                   | Type              | Example                | Description                                            |   
+| --------------------- | ----------------- | ---------------------- | ------------------------------------------------------ |
+| AssetPropWhitelistIn  | String            | parameter1             | White list of parameters which will be accepted        |
+| AssetPropWhitelistOut | String            | parameter2             | White list of parameters which will be in the response |
+| AssetPropMapping      | (String String)[] | parent#child mappedKey | Transforms the database keys to a mapped key           |
 
 
 **User**
 
-| Key                             | Type              | Example                | Description                                            |
-| ------------------------------- | ----------------- | ---------------------- | ------------------------------------------------------ |
-| UserProfileParamInputWhitelist  | String[]          | parameter1             | White list of parameters which will be accepted        |
-| UserProfileParamOutputWhitelist | String[]          | parameter2             | White list of parameters which will be in the response |
-| UserProfileParamMapping         | (String String)[] | parent#child mappedKey | Transforms the database keys to a mapped key           |
+| Key                  | Type              | Example                | Description                                            |
+| -------------------- | ----------------- | ---------------------- | ------------------------------------------------------ |
+| UserPropWhitelistIn  | String[]          | parameter1             | White list of parameters which will be accepted        |
+| UserPropWhitelistOut | String[]          | parameter2             | White list of parameters which will be in the response |
+| UserPropMapping      | (String String)[] | parent#child mappedKey | Transforms the database keys to a mapped key           |
 
 
 ##Query Lists
@@ -74,8 +74,8 @@ To set up the database following schema can be used:
 
 | Key            | Type           | Example                  | Description                                                    |   
 | -------------- | -------------- | ------------------------ | -------------------------------------------------------------- |
-| AssetStorePath | Path           | /var/www/html/assetstore | Path where the assets shall be stored                          |
-| AssetTmpPath   | Path           | /tmp/asset               | Path and file-prefix to store assets temporary while uploading |
+| AssetPathStore | Path           | /var/www/html/assetstore | Path where the assets shall be stored                          |
+| AssetPathTmp   | Path           | /tmp/asset               | Path and file-prefix to store assets temporary while uploading |
 | AssetTransform | (String Int)[] | large 1000               | Setting names for image transformation widths                  |
 
 
--- mongocms/module/directives.c
+++ mongocms/module/directives.c
@@ -14,30 +14,30 @@ const char *directives_parseCreateList(cmd_parms *cmd, void *mconfig, const char
 const char DocMongoURI[] = "DocMongoURI";
 const char DocMongoDB[] = "DocMongoDB";
 const char DocMongoCollection[] = "DocMongoCollection";
-const char DocumentPropInputWhitelist[] = "DocumentPropInputWhitelist";
-const char DocumentPropOutputWhitelist[] = "DocumentPropOutputWhitelist";
+const char DocumentPropWhitelistIn[] = "DocumentPropWhitelistIn";
+const char DocumentPropWhitlistOut[] = "DocumentPropWhitlistOut";
 const char DocumentPropMapping[] = "DocumentPropMapping";
 const char DocumentQueryList[] = "DocumentQueryList";
 // Assets
 const char AssetMongoURI[] = "AssetMongoURI";
 const char AssetMongoDB[] = "AssetMongoDB";
 const char AssetMongoCollection[] = "AssetMongoCollection";
-const char AssetStorePath[] = "AssetStorePath";
-const char AssetTmpPath[] = "AssetTmpPath";
+const char AssetPathStore[] = "AssetPathStore";
+const char AssetPathTmp[] = "AssetPathTmp";
 const char AssetQueryList[] = "AssetQueryList";
 const char AssetTransform[] = "AssetTransform";
 
-const char AssetMetadataParamMapping[] = "AssetMetadataParamMapping";
-const char AssetMetadataParamInputWhitelist[] = "AssetMetadataParamInputWhitelist";
-const char AssetMetadataParamOutputWhitelist[] = "AssetMetadataParamOutputWhitelist";
+const char AssetPropMapping[] = "AssetPropMapping";
+const char AssetPropWhitelistIn[] = "AssetPropWhitelistIn";
+const char AssetPropWhitelistOut[] = "AssetPropWhitelistOut";
 
 // User Management
 const char UserMongoURI[] = "UserMongoURI";
 const char UserMongoDB[] = "UserMongoDB";
 const char UserMongoCollection[] = "UserMongoCollection";
-const char UserProfileParamMapping[] = "UserProfileParamMapping";
-const char UserProfileParamInputWhitelist[] = "UserProfileParamInputWhitelist";
-const char UserProfileParamOutputWhitelist[] = "UserProfileParamOutputWhitelist";
+const char UserPropMapping[] = "UserPropMapping";
+const char UserPropWhitelistIn[] = "UserPropWhitelistIn";
+const char UserPropWhitelistOut[] = "UserPropWhitelistOut";
 const char UserSessionsMaxCount[] = "UserSessionsMaxCount";
 const char UserSessionExpirationTime[] = "UserSessionExpirationTime";
 const char UserQueryList[] = "UserQueryList";
@@ -52,8 +52,8 @@ command_rec mongocms_directives[] = {
 		AP_INIT_TAKE1(DocMongoCollection, directives_parserString, NULL, RSRC_CONF, ""),
 
 		// Document-Handling
-		AP_INIT_TAKE1(DocumentPropInputWhitelist, directives_parserStringList, NULL, RSRC_CONF, ""),
-		AP_INIT_TAKE1(DocumentPropOutputWhitelist, directives_parserStringList, NULL, RSRC_CONF, ""),
+		AP_INIT_TAKE1(DocumentPropWhitelistIn, directives_parserStringList, NULL, RSRC_CONF, ""),
+		AP_INIT_TAKE1(DocumentPropWhitlistOut, directives_parserStringList, NULL, RSRC_CONF, ""),
 		AP_INIT_TAKE2(DocumentPropMapping, directives_parserTransformator, NULL, RSRC_CONF, ""),
 		AP_INIT_RAW_ARGS(DocumentQueryList, directives_parseCreateList, NULL, RSRC_CONF, ""),
 
@@ -64,12 +64,12 @@ command_rec mongocms_directives[] = {
 		AP_INIT_TAKE1(AssetMongoCollection, directives_parserString, NULL, RSRC_CONF, ""),
 
 		// Assets
-		AP_INIT_TAKE1(AssetStorePath, directives_parserString, NULL, RSRC_CONF, ""),
-		AP_INIT_TAKE1(AssetTmpPath, directives_parserString, NULL, RSRC_CONF, ""),
+		AP_INIT_TAKE1(AssetPathStore, directives_parserString, NULL, RSRC_CONF, ""),
+		AP_INIT_TAKE1(AssetPathTmp, directives_parserString, NULL, RSRC_CONF, ""),
 		AP_INIT_TAKE2(AssetTransform, directives_parserSpecialAssetTransform, NULL, RSRC_CONF, ""),
-		AP_INIT_TAKE1(AssetMetadataParamInputWhitelist, directives_parserStringList, NULL, RSRC_CONF, ""),
-		AP_INIT_TAKE1(AssetMetadataParamOutputWhitelist, directives_parserStringList, NULL, RSRC_CONF, ""),
-		AP_INIT_TAKE2(AssetMetadataParamMapping, directives_parserTransformator, NULL, RSRC_CONF, ""),
+		AP_INIT_TAKE1(AssetPropWhitelistIn, directives_parserStringList, NULL, RSRC_CONF, ""),
+		AP_INIT_TAKE1(AssetPropWhitelistOut, directives_parserStringList, NULL, RSRC_CONF, ""),
+		AP_INIT_TAKE2(AssetPropMapping, directives_parserTransformator, NULL, RSRC_CONF, ""),
 		AP_INIT_RAW_ARGS(AssetQueryList, directives_parseCreateList, NULL, RSRC_CONF, ""),
 
 		// User
@@ -77,9 +77,9 @@ command_rec mongocms_directives[] = {
 		AP_INIT_TAKE1(UserMongoURI, directives_parserString, NULL, RSRC_CONF, ""),
 		AP_INIT_TAKE1(UserMongoDB, directives_parserString, NULL, RSRC_CONF, ""),
 		AP_INIT_TAKE1(UserMongoCollection, directives_parserString, NULL, RSRC_CONF, ""),
-		AP_INIT_TAKE2(UserProfileParamMapping, directives_parserTransformator, NULL, RSRC_CONF, ""),
-		AP_INIT_TAKE1(UserProfileParamInputWhitelist, directives_parserStringList, NULL, RSRC_CONF, ""),
-		AP_INIT_TAKE1(UserProfileParamOutputWhitelist, directives_parserStringList, NULL, RSRC_CONF, ""),
+		AP_INIT_TAKE2(UserPropMapping, directives_parserTransformator, NULL, RSRC_CONF, ""),
+		AP_INIT_TAKE1(UserPropWhitelistIn, directives_parserStringList, NULL, RSRC_CONF, ""),
+		AP_INIT_TAKE1(UserPropWhitelistOut, directives_parserStringList, NULL, RSRC_CONF, ""),
 		AP_INIT_TAKE1(UserSessionsMaxCount, directives_parserInt, NULL, RSRC_CONF, ""),
 		AP_INIT_TAKE1(UserSessionExpirationTime, directives_parserInt, NULL, RSRC_CONF, ""),
 		AP_INIT_RAW_ARGS(UserQueryList, directives_parseCreateList, NULL, RSRC_CONF, ""),
@@ -97,8 +97,8 @@ const char *directives_parserString(cmd_parms *cmd, void *cfg, const char *arg) 
 	DIRECTIVES_MODULE_HELPER(UserMongoURI) getModuleConfig()->user.database.uri = arg;
 	DIRECTIVES_MODULE_HELPER(UserMongoDB) getModuleConfig()->user.database.db = arg;
 	DIRECTIVES_MODULE_HELPER(UserMongoCollection) getModuleConfig()->user.database.collection = arg;
-	DIRECTIVES_MODULE_HELPER(AssetStorePath) getModuleConfig()->asset.assetstorepath = arg;
-	DIRECTIVES_MODULE_HELPER(AssetTmpPath) getModuleConfig()->asset.assettmppath = arg;
+	DIRECTIVES_MODULE_HELPER(AssetPathStore) getModuleConfig()->asset.assetstorepath = arg;
+	DIRECTIVES_MODULE_HELPER(AssetPathTmp) getModuleConfig()->asset.assettmppath = arg;
 	return NULL;
 }
 
@@ -112,12 +112,12 @@ const char *directives_parserInt(cmd_parms *cmd, void *cfg, const char *arg) {
 
 const char *directives_parserStringList(cmd_parms *cmd, void *cfg, const char *arg) {
 	apr_array_header_t **arr = NULL;
-	DIRECTIVES_MODULE_HELPER(DocumentPropInputWhitelist) arr = &getModuleConfig()->document.propWhitelistIn;
-	DIRECTIVES_MODULE_HELPER(DocumentPropOutputWhitelist) arr = &getModuleConfig()->document.propWhitelistOut;
-	DIRECTIVES_MODULE_HELPER(AssetMetadataParamInputWhitelist) arr = &getModuleConfig()->asset.propWhitelistIn;
-	DIRECTIVES_MODULE_HELPER(AssetMetadataParamOutputWhitelist) arr = &getModuleConfig()->asset.propWhitelistOut;
-	DIRECTIVES_MODULE_HELPER(UserProfileParamInputWhitelist) arr = &getModuleConfig()->user.propWhitelistIn;
-	DIRECTIVES_MODULE_HELPER(UserProfileParamOutputWhitelist) arr = &getModuleConfig()->user.propWhitelistOut;
+	DIRECTIVES_MODULE_HELPER(DocumentPropWhitelistIn) arr = &getModuleConfig()->document.propWhitelistIn;
+	DIRECTIVES_MODULE_HELPER(DocumentPropWhitlistOut) arr = &getModuleConfig()->document.propWhitelistOut;
+	DIRECTIVES_MODULE_HELPER(AssetPropWhitelistIn) arr = &getModuleConfig()->asset.propWhitelistIn;
+	DIRECTIVES_MODULE_HELPER(AssetPropWhitelistOut) arr = &getModuleConfig()->asset.propWhitelistOut;
+	DIRECTIVES_MODULE_HELPER(UserPropWhitelistIn) arr = &getModuleConfig()->user.propWhitelistIn;
+	DIRECTIVES_MODULE_HELPER(UserPropWhitelistOut) arr = &getModuleConfig()->user.propWhitelistOut;
 	
 	if ( *arr == NULL ) {
 		*arr = apr_array_make(getModuleConfig()->modulePool, CONFIG_ARRAY_INIT_SIZE, sizeof(char *));
@@ -133,12 +133,12 @@ const char *directives_parserTransformator(cmd_parms *cmd, void *cfg, const char
 	apr_table_t **transformOut = NULL;
 	apr_table_t **transformIn = NULL;
 	
-	DIRECTIVES_MODULE_HELPER(AssetMetadataParamMapping) {
+	DIRECTIVES_MODULE_HELPER(AssetPropMapping) {
 		transformOut = &getModuleConfig()->asset.propMappingOut;
 		transformIn = &getModuleConfig()->asset.propMappingIn;
 	}
 
-	DIRECTIVES_MODULE_HELPER(UserProfileParamMapping) {
+	DIRECTIVES_MODULE_HELPER(UserPropMapping) {
 		transformOut = &getModuleConfig()->user.propMappingOut;
 		transformIn = &getModuleConfig()->user.propMappingIn;
 	}
