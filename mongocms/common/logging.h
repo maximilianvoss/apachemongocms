832bf899cba187904a4fe203a0f58a445efd2a5e
Maximilian Voss <maximilian.voss@gmail.com>
2017-06-07 20:50:08 +0200

    request bound logging

--- mongocms/common/logging.h
+++ mongocms/common/logging.h
@@ -7,17 +7,16 @@
 #define LOG_REQUEST_ERROR(file, line, status, request, fmt, ...) ap_log_rerror(file, line, 1, 1, status, request, fmt, __VA_ARGS__)
 #define LOG_SERVER_DEBUG_FORMAT(fmt, ...) if(getModuleConfig()->serverRec != NULL) ap_log_error("Unknown File", 0, 1, 1, 99, getModuleConfig()->serverRec, fmt, __VA_ARGS__)
 
-//// LOGGING LEVEL
-//// ERROR    0
-//// WARN     1
-//// INFO     2
-//// DEBUG    3
-//// TRACE    4
+#define LOGGING_ERROR_S(fmt, ...) if(getModuleConfig()->serverRec != NULL) ap_log_error(__FILE__, __LINE__, 1, APLOG_ERR, 0, getModuleConfig()->serverRec, fmt, ##__VA_ARGS__)
+#define LOGGING_WARN_S(fmt, ...) if(getModuleConfig()->serverRec != NULL) ap_log_error(__FILE__, __LINE__, 1, APLOG_WARNING, APR_SUCCESS, getModuleConfig()->serverRec, fmt, ##__VA_ARGS__)
+#define LOGGING_INFO_S(fmt, ...) if(getModuleConfig()->serverRec != NULL) ap_log_error(__FILE__, __LINE__, 1, APLOG_INFO, APR_SUCCESS, getModuleConfig()->serverRec, fmt, ##__VA_ARGS__)
+#define LOGGING_DEBUG_S(fmt, ...) if(getModuleConfig()->serverRec != NULL) ap_log_error(__FILE__, __LINE__, 1, APLOG_DEBUG, APR_SUCCESS, getModuleConfig()->serverRec, fmt, ##__VA_ARGS__)
+#define LOGGING_TRACE_S(fmt, ...) if(getModuleConfig()->serverRec != NULL) ap_log_error(__FILE__, __LINE__, 1, APLOG_TRACE1, APR_SUCCESS, getModuleConfig()->serverRec, fmt, ##__VA_ARGS__)
 
-#define LOGGING_ERROR(fmt, ...) if(getModuleConfig()->serverRec != NULL) ap_log_error(__FILE__, __LINE__, 1, APLOG_ERR, 0, getModuleConfig()->serverRec, fmt, ##__VA_ARGS__)
-#define LOGGING_WARN(fmt, ...) if(getModuleConfig()->serverRec != NULL) ap_log_error(__FILE__, __LINE__, 1, APLOG_WARNING, APR_SUCCESS, getModuleConfig()->serverRec, fmt, ##__VA_ARGS__)
-#define LOGGING_INFO(fmt, ...) if(getModuleConfig()->serverRec != NULL) ap_log_error(__FILE__, __LINE__, 1, APLOG_INFO, APR_SUCCESS, getModuleConfig()->serverRec, fmt, ##__VA_ARGS__)
-#define LOGGING_DEBUG(fmt, ...) if(getModuleConfig()->serverRec != NULL) ap_log_error(__FILE__, __LINE__, 1, APLOG_DEBUG, APR_SUCCESS, getModuleConfig()->serverRec, fmt, ##__VA_ARGS__)
-#define LOGGING_TRACE(fmt, ...) if(getModuleConfig()->serverRec != NULL) ap_log_error(__FILE__, __LINE__, 1, APLOG_TRACE1, APR_SUCCESS, getModuleConfig()->serverRec, fmt, ##__VA_ARGS__)
+#define LOGGING_ERROR_R(request, fmt, ...) if(request != NULL) ap_log_rerror(__FILE__, __LINE__, 1, APLOG_ERR, 0, request, fmt, ##__VA_ARGS__)
+#define LOGGING_WARN_R(request, fmt, ...) if(request != NULL) ap_log_rerror(__FILE__, __LINE__, 1, APLOG_WARNING, 0, request, fmt, ##__VA_ARGS__)
+#define LOGGING_INFO_R(request, fmt, ...) if(request != NULL) ap_log_rerror(__FILE__, __LINE__, 1, APLOG_INFO, 0, request, fmt, ##__VA_ARGS__)
+#define LOGGING_DEBUG_R(request, fmt, ...) if(request != NULL) ap_log_rerror(__FILE__, __LINE__, 1, APLOG_DEBUG, 0, request, fmt, ##__VA_ARGS__)
+#define LOGGING_TRACE_R(request, fmt, ...) if(request != NULL) ap_log_rerror(__FILE__, __LINE__, 1, APLOG_TRACE1, 0, request, fmt, ##__VA_ARGS__)
 
 #endif
--- mongocms/module/requesthandler.c
+++ mongocms/module/requesthandler.c
@@ -9,8 +9,9 @@
 #include "../common/logging.h"
 
 int request_handler(request_rec *request) {
-	LOGGING_ERROR("huhu");
+	LOGGING_DEBUG_R(request, "START");
 	if ( strcmp(request->handler, "mongocms") ) {
+		LOGGING_DEBUG_R(request, "DONE");
 		return DECLINED;
 	}
 
@@ -19,19 +20,31 @@ int request_handler(request_rec *request) {
 	// check for valid user credentials for doing a post
 	if ( request->method_number == M_POST || request->method_number == M_PUT ) {
 		if ( ( !( !strcmp(filename, FILENAME_USER_LOGIN) || !strcmp(filename, FILENAME_USER_REGISTRATION) ) && !user_isLoginValid(request) ) ) {
+			LOGGING_INFO_R(request, "no valid user credentials");
+			LOGGING_DEBUG_R(request, "DONE");
 			return DECLINED;
 		}
 	}
-
+	
+	int retVal;
 	// check for user/login/profile requests
 	if ( !strcmp(filename, FILENAME_USER_LOGIN) || !strcmp(filename, FILENAME_USER_LOGOUT) || !strcmp(filename, FILENAME_USER_PROFILE) || !strcmp(filename, FILENAME_USER_REGISTRATION) ||
 	     stringutil_startsWith(request->uri, "/user") ) {
-		return user_handler(request, filename);
+		LOGGING_INFO_R(request, "request for user profile");
+		retVal = user_handler(request, filename);
+		LOGGING_DEBUG_R(request, "DONE");
+		return retVal;
 	}
 
 	if ( stringutil_startsWith(request->uri, "/dam") ) {
-		return assets_handler(request, filename);
+		LOGGING_INFO_R(request, "request for dam");
+		retVal = assets_handler(request, filename);
+		LOGGING_DEBUG_R(request, "DONE");
+		return retVal;
 	}
 
-	return documents_handler(request, filename);
+	LOGGING_INFO_R(request, "request for document");
+	retVal = documents_handler(request, filename);
+	LOGGING_DEBUG_R(request, "DONE");
+	return retVal;
 }
